a9702ee4d8f31125a305d3c6620bfd0a2ce9e739
MCrank <marco_mcse@hotmail.com>
2020-02-09 09:49:08 -0600

    Updated to latest version, added some checksum matching to prevent re-download

--- Install-EveBackendReqs.ps1
+++ Install-EveBackendReqs.ps1
@@ -3,7 +3,7 @@
 # Created by:   Marco Crank
 # Organization: Nashville Software School
 # Filename:     Install-EveBackendReqs.ps1
-# Version:      2.0
+# Version:      3.0
 # Disclaimer:   THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
 #               ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
 #               THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
@@ -22,6 +22,8 @@
 #
 # Updates:      1.0 - (02/21/2019) Initial Release
 #               2.0 - (08/15/2019) Updated for 2019, Added .Net Core SDK
+#               3.0 - (02/08/2020) Updated dotnet (3.1), Updated SQL 2019 Dev, Remove KDiff
+#                     Added file hash matching to prevent re-download of files
 #========================================================================
 
 #requires -RunAsAdministrator
@@ -35,36 +37,41 @@ Clear-Host
 $RootFolderPath = 'C:\Nss_Install'
 
 # *** SSMS ***
-$SsmsRequiredVersion = '15.0.18142.0' # SSMS 17 RTM (https://sqlserverbuilds.blogspot.com/2018/01/sql-server-management-studio-ssms.html)
-$SsmsFolderPath = "$RootFolderPath\SSMS" #SSMS-Setup-ENU.exe
-$SsmsUrl = 'https://go.microsoft.com/fwlink/?linkid=2099720'
+$SsmsRequiredVersion = '15.0.18206.0' # SSMS 18 RTM (https://sqlserverbuilds.blogspot.com/2018/01/sql-server-management-studio-ssms.html)
+$SsmsFolderPath = "$RootFolderPath\SSMS"
+$SsmsInstallerHash = '5A0BCF1665C56B4EE839361CD1AB44AB7864A9361BC60C508B0100D00E58A5E2' # SHA256
+$SsmsUrl = 'https://download.microsoft.com/download/1/9/4/1949aa9c-6536-48f2-81fa-e7bb07410b36/SSMS-Setup-ENU.exe'
+$SsmsExecutable = ([uri]$SsmsUrl | Select-Object -ExpandProperty Segments)[([uri]$SsmsUrl | Select-Object -ExpandProperty Segments).length - 1]
 
 # *** SQL Developer Edition ***
-$SqlRequiredVersion = '14.0.1000.169' # SQL 2017 RTM (http://sqlserverbuilds.blogspot.com/)
-$SqlFolderPath = "$RootFolderPath\SQL" # SQLServer2017-SSEI-Dev.exe
-$SqlUrl = 'https://go.microsoft.com/fwlink/?linkid=853016'
+$SqlRequiredVersion = '15.0.2000.5' # SQL 2019 RTM (http://sqlserverbuilds.blogspot.com/)
+$SqlFolderPath = "$RootFolderPath\SQL"
+$SqlInstallerHash = 'FB67DB0057C0229F3A13AFCE79FA12F0926C644F8525AA27922FF53FA0305F6F' # SHA256
+$SqlUrl = 'https://download.microsoft.com/download/b/8/c/b8ce1000-2e0b-4bc8-b4e4-646e9a439525/SQL2019-SSEI-Dev.exe'
+$SqlExecutable = ([uri]$SqlUrl | Select-Object -ExpandProperty Segments)[([uri]$SqlUrl | Select-Object -ExpandProperty Segments).length - 1]
 $SqlConfigUrl = 'https://bit.ly/2GTSp36'
 
 # *** VS Community Edition 2019 ***
 $VsFolderPath = "$RootFolderPath\VS" # vs_community.exe
+$VsInstallerHash = '9BE9BFB28D3A96554915A4BEDF5FE733BC4C832B1079F3C884E138AFD21DD118' # SHA256
 $VsCommunityUrl = 'https://download.visualstudio.microsoft.com/download/pr/818029a2-ea31-4a6a-8bed-f50abbaf9716/ff2d0f080b97ad9de29126e301f93a26/vs_community.exe'
+$VsExecutable = ([uri]$VsCommunityUrl | Select-Object -ExpandProperty Segments)[([uri]$VsCommunityUrl | Select-Object -ExpandProperty Segments).length - 1]
 
-# *** .Net Core 2.2.401 SDK (VS 2019 Version)
+# *** .Net Core 3.1.101 SDK
 # Hardcoding to this version as it is the latest.  No easy way to just get the latest because Microsoft is stupid with the versioning of .net core now
 #   and grabbing the release from Github...Well yeah all the links are in markdown files so this is what you get
 $DotNetFolderPath = "$RootFolderPath\DotNetCore"
-$DotNetUrl = 'https://download.visualstudio.microsoft.com/download/pr/c76aa823-bbc7-4b21-9e29-ab24ceb14b2d/9de2e14be600ef7d5067c09ab8af5063/dotnet-sdk-2.2.401-win-x64.exe'
-
-# *** KDiff3 ***
-# Hasn't been updated in many moons
-$Kdiff3FolderPath = "$RootFolderPath\KDiff" #KDiff3-64bit-Setup_0.9.98-2.exe
-$Kdiff3Url = 'https://svwh.dl.sourceforge.net/project/kdiff3/kdiff3/0.9.98/KDiff3-64bit-Setup_0.9.98-2.exe'
+$DotNetHash = "eec02a5434de65b36b481136db06cd10eba4ca5c4947b536d567b003a8cac4f29012b74ad131a1e0dd4a79611702aa660f57949f0761259d1050d2481e4929cd" # SHA512
+$DotNetUrl = 'https://download.visualstudio.microsoft.com/download/pr/854ca330-4414-4141-9be8-5da3c4be8d04/3792eafd60099b3050313f2edfd31805/dotnet-sdk-3.1.101-win-x64.exe'
+$DotNetExecutableName = ([uri]$DotNetUrl | Select-Object -ExpandProperty Segments)[([uri]$DotNetUrl | Select-Object -ExpandProperty Segments).length - 1]
 
 # *** Git ***
 # Git is a bit different since they have version in the name.  Let's grab the latest from the API
 $LatestGitFerWindersRelease = (Invoke-WebRequest 'https://api.github.com/repos/git-for-windows/git/releases/latest' -UseBasicParsing -Headers @{"Accept" = "application/json" }).Content | ConvertFrom-Json
 $GitDownloadUrl = ($LatestGitFerWindersRelease.assets | Where-Object { $_.Name -like "Git-*-64-bit.exe" }).browser_download_url
 $GitName = ($LatestGitFerWindersRelease.assets | Where-Object { $_.Name -like "Git-*-64-bit.exe" }).name
+$GitHashStartIndex = $LatestGitFerWindersRelease.body.IndexOf($GitName)
+$GitHash = $LatestGitFerWindersRelease.body.Substring($GitHashStartIndex, ($LatestGitFerWindersRelease.body.IndexOf("`n", $GitHashStartIndex) - $GitHashStartIndex)).split("|").trim()[1]
 $GitConfigIniUrl = 'https://bit.ly/31Bi2Nl'
 $GitFolderPath = "$RootFolderPath\Git"
 
@@ -90,7 +97,7 @@ function New-NSSFolders ()
   Param([string]$Path)
   if (!(Test-Path -Path $Path))
   {
-    New-Item -Path $Path -ItemType Directory -Force
+    $null = New-Item -Path $Path -ItemType Directory -Force
   }
 }
 
@@ -98,28 +105,44 @@ function Confirm-VisualStudio ()
 {
   if (Get-Package -Name "Visual Studio Community 2019" -ProviderName Programs -ErrorAction SilentlyContinue)
   {
-    Out-Console -Message "Visual Studio Already Installed" -Mode Check
+    Out-Console -Message "`t- Visual Studio Already Installed" -Mode Check
     return $true
   }
   else
   {
-    Out-Console -Message "Visual Studio not installed. Downloading Installation." -Mode Check
-    Start-BitsTransfer -Source $VsCommunityUrl -Destination "$VsFolderPath\vs_community.exe"
+    if (!(Compare-Checksum -SourceHash $VsInstallerHash -TargetPath "$VsFolderPath\$VsExecutable" -Algorithm SHA256))
+    {
+      Out-Console -Message "`t- Visual Studio not installed. Downloading and Installing. Please Wait..." -Mode Check
+      Start-BitsTransfer -Source $VsCommunityUrl -Destination $VsFolderPath
+    }
+    else
+    {
+      Out-Console -Message "`t- Visual Studio Installer found locally. Installing..." -Mode Check
+
+    }
     return $false
   }
 }
 
 function Confirm-DotnetCore
 {
-  if (Get-Package -ProviderName Programs -ErrorAction SilentlyContinue | Where-Object { $_.Name -like "*Microsoft .NET Core SDK 2.2*" -and $_.Version -ge '2.2.401' })
+  $DotNetVersion = $DotNetExecutableName -match "\d+(\.\d+)+" | ForEach-Object { $Matches[0] }
+  if (Get-Package -ProviderName Programs -ErrorAction SilentlyContinue | Where-Object { $_.Name -like "*Microsoft .NET Core SDK*" -and $_.Version -ge $DotNetVersion })
   {
-    Out-Console -Message "Required .Net Core SDK Already Installed" -Mode Check
+    Out-Console -Message "`t- Required .Net Core SDK Already Installed" -Mode Check
     return $true
   }
   else
   {
-    Out-Console -Message "Required .Net Core SDK not found. Downloading Installation" -Mode Check
-    Start-BitsTransfer -Source $DotNetUrl -Destination "$DotNetFolderPath\dotnet-sdk-2.2.401-win-x64.exe"
+    if (!(Compare-Checksum -SourceHash $DotNetHash -TargetPath "$DotNetFolderPath\$DotNetExecutableName" -Algorithm SHA512))
+    {
+      Out-Console -Message "`t- Required .Net Core SDK not found. Downloading Installation..." -Mode Check
+      Start-BitsTransfer -Source $DotNetUrl -Destination $DotNetFolderPath
+    }
+    else
+    {
+      Out-Console -Message "`t- DotNet Core installer found locally. Installing..." -Mode Check
+    }
     return $false
   }
 }
@@ -129,10 +152,18 @@ function Confirm-SqlInstall ()
   $SqlInstances = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server' -ErrorAction SilentlyContinue).InstalledInstances
   if (!($SqlInstances -contains "MSSQLSERVER"))
   {
-    # Default instance not found so
-    Out-Console -Message "SQL 2017 Instance not found. Downloading Installation" -Mode Check
-    Start-BitsTransfer -Source $SqlConfigUrl -Destination "$SqlFolderPath\ConfigurationFile.ini"
-    Start-BitsTransfer -Source $SqlUrl -Destination "$SqlFolderPath\SQLServer2017-SSEI-Dev.exe"
+    if (!(Compare-Checksum -SourceHash $SqlInstallerHash -TargetPath "$SqlFolderPath\$SqlExecutable" -Algorithm SHA256))
+    {
+      # Default instance not found so
+      Out-Console -Message "`t- SQL 2019 Instance not found. Downloading Installation..." -Mode Check
+      Start-BitsTransfer -Source $SqlConfigUrl -Destination "$SqlFolderPath\ConfigurationFile.ini"
+    }
+    else
+    {
+      Out-Console -Message "`t- SQL Server Installer found locally.  Installing..." -Mode Check
+    }
+    # Grabbing the config file no matter what
+    Start-BitsTransfer -Source $SqlUrl -Destination $SqlFolderPath
     return $false
   }
   else
@@ -141,12 +172,12 @@ function Confirm-SqlInstall ()
     $SqlDefaultInstance = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL').MSSQLSERVER
     if ((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$SqlDefaultInstance\Setup").Version -ge $SqlRequiredVersion)
     {
-      Out-Console -Message "SQL Server Already Installed" -Mode Check
+      Out-Console -Message "`t- SQL Server Already Installed" -Mode Check
       return $true
     }
     else
     {
-      # Found a default instance but it is lower version than SQL 2017.  Manual upgrade
+      # Found a default instance but it is lower version than SQL 2019.  Manual upgrade
       Out-Console -Message "***** Older SQL server version found please upgrade manually ******" -Mode Error
       return $true
     }
@@ -158,60 +189,70 @@ function Confirm-SSMS ()
   $SSMSVersion = (Get-Package -Name "Microsoft SQL Server Management Studio*" -ProviderName 'Programs' -ErrorAction SilentlyContinue).Version
   if ($SSMSVersion -ge $SsmsRequiredVersion)
   {
-    Out-Console -Message "SSMS Already Installed" -Mode Check
+    Out-Console -Message "`t- SSMS Already Installed" -Mode Check
     return $true
   }
   else
   {
-    Out-Console -Message "SSMS version not met. Downloading Installation" -Mode Check
-    Start-BitsTransfer -Source $SsmsUrl -Destination "$SsmsFolderPath\SSMS-Setup-ENU.exe"
+    if (!(Compare-Checksum -SourceHash $SsmsInstallerHash -TargetPath "$SsmsFolderPath\$SsmsExecutable" -Algorithm SHA256))
+    {
+      Out-Console -Message "`t- SSMS version not met. Downloading Installation" -Mode Check
+      Start-BitsTransfer -Source $SsmsUrl -Destination $SsmsFolderPath
+    }
+    else
+    {
+      Out-Console -Message "`t- SSMS Installater found locally.  Installing..." -Mode Check
+    }
     return $false
   }
 }
 
-function Confirm-Kdiff
-{
-  if (Get-Package -Name *Kdiff* -ProviderName Programs -ErrorAction SilentlyContinue)
-  {
-    Out-Console -Message "KDiff Already Installed" -Mode Check
-    return $true
-  }
-  else
-  {
-    Start-BitsTransfer -Source $Kdiff3Url -Destination "$Kdiff3FolderPath\KDiff3-64bit-Setup_0.9.98-2.exe"
-    return $false
-  }
-}
-
-function Confirm-Git
+function Confirm-Git ()
 {
   if (Get-Package -Name *git* -ProviderName Programs -ErrorAction SilentlyContinue)
   {
-    Out-Console -Message "Git for Windows already installed" -Mode Check
+    Out-Console -Message "`t- Git for Windows already installed" -Mode Check
     return $true
   }
   else
   {
-    Start-Process bitsadmin.exe -ArgumentList " /transfer GitDownload /dynamic /priority FOREGROUND $GitDownloadUrl $GitFolderPath\$GitName" -WindowStyle Hidden -Wait
+    if (!(Compare-Checksum -SourceHash $GitHash -TargetPath "$GitFolderPath\$GitName" -Algorithm SHA256))
+    {
+      Out-Console -Message "`t- Git not installed. Downloading" -Mode Check
+      Start-Process bitsadmin.exe -ArgumentList " /transfer GitDownload /dynamic /priority FOREGROUND $GitDownloadUrl $GitFolderPath\$GitName" -WindowStyle Hidden -Wait
+    }
+    else
+    {
+      Out-Console -Message "`t- Git Installer found locally. Installing..." -Mode Check
+    }
+    # Grab the got config no matter what
     Invoke-WebRequest -Uri $GitConfigIniUrl -OutFile "$GitFolderPath\GitConfig.ini"
     return $false
   }
 }
 
-function Confirm-GitConfig
+function Compare-Checksum ()
 {
-  Param ([string]$Command)
-  # merge.tool=kdiff3
-  # mergetool.kdiff3.path=C:/Program Files/KDiff3/kdiff3.exe
-  Start-Process -FilePath "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ArgumentList "git config --get $Command" -RedirectStandardOutput 'C:\Nss_Install\check_gitconfig.txt' -NoNewWindow -Wait
-  $Result = Get-Content -Path  'C:\Nss_Install\check_gitconfig.txt'
-  if ($Result -eq 'kdiff3')
+  Param(
+    [string]$SourceHash,
+    [string]$TargetPath,
+    [string]$Algorithm
+  )
+  if (!(Test-Path -Path $TargetPath))
   {
-    return $true
+    return $false
   }
   else
   {
-    return $false
+    $TargetHash = (Get-FileHash -Path $TargetPath -Algorithm $Algorithm).Hash
+    if ($SourceHash -eq $TargetHash)
+    {
+      return $true
+    }
+    else
+    {
+      return $false
+    }
   }
 }
 
@@ -223,7 +264,7 @@ Out-Console -Message "Checking Operating System Architecture"
 $OSArchitecture = (Get-CimInstance -ClassName Win32_OperatingSystem).OSArchitecture
 if ($OSArchitecture -ne "64-Bit")
 {
-  Out-Console -Message "This script requires you to be running 64-Bit Windows. NSS requirements need manual install" -Mode Error
+  Out-Console -Message "This script requires you to be running 64-Bit Windows. NSS C# Backend requirements need to be manually installed" -Mode Error
   break
 }
 
@@ -236,7 +277,7 @@ foreach ($Path in ($FolderPaths | Sort-Object -Property 'Value'))
 }
 
 Out-Console -Message "Installing Git for Windows"
-# Check if git is installed, At this point it should be unless a fresh install
+# Check if git is installed, At this point it should be from Front End unless a fresh install
 if (!(Confirm-Git))
 {
   Start-Process -FilePath "$GitFolderPath\$GitName" -ArgumentList "/VERYSILENT /LOADINF=GitConfig.inf /NORESTART" -NoNewWindow -Wait
@@ -250,40 +291,25 @@ Out-Console -Message "Installing Visual Studio 2019 Community"
 # https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2019#error-codes
 if (!(Confirm-VisualStudio))
 {
-  Start-Process -FilePath "$VsFolderPath\vs_community.exe" -ArgumentList "--includeRecommended --quiet --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --norestart" -NoNewWindow -Wait
+  Start-Process -FilePath "$VsFolderPath\$VsExecutable" -ArgumentList "--includeRecommended --quiet --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --norestart" -NoNewWindow -Wait
 }
 
 Out-Console -Message "Installing .Net Core SDK"
 if (!(Confirm-DotnetCore))
 {
-  Start-Process -FilePath "$DotNetFolderPath\dotnet-sdk-2.2.401-win-x64.exe" -ArgumentList "/quiet /norestart /log $DotNetFolderPath\dotnet_install.txt" -NoNewWindow -Wait
+  Start-Process -FilePath "$DotNetFolderPath\$DotNetExecutableName" -ArgumentList "/quiet /norestart /log $DotNetFolderPath\dotnet_install.txt" -NoNewWindow -Wait
 }
 
-Out-Console -Message "Downloading SQL Server 2017 Developer Edition Bootstrap files"
+Out-Console -Message "Downloading SQL Server 2019 Developer Edition Bootstrap files"
 if (!(Confirm-SqlInstall))
 {
-  Start-Process -FilePath "$SqlFolderPath\SQLServer2017-SSEI-Dev.exe" -ArgumentList "/ACTION=Install /CONFIGURATIONFILE=$SqlFolderPath\ConfigurationFile.ini /IACCEPTSQLSERVERLICENSETERMS /MEDIAPATH=$SqlFolderPath /QUIET" -NoNewWindow -Wait
+  Start-Process -FilePath "$SqlFolderPath\$SqlExecutable" -ArgumentList "/ACTION=Install /CONFIGURATIONFILE=$SqlFolderPath\ConfigurationFile.ini /IACCEPTSQLSERVERLICENSETERMS /MEDIAPATH=$SqlFolderPath /QUIET" -NoNewWindow -Wait
 }
 
 Out-Console -Message "Installing SQL Server Management Studio (SSMS)"
 if (!(Confirm-SSMS))
 {
-  Start-Process -FilePath "$SsmsFolderPath\SSMS-Setup-ENU.exe" -ArgumentList "/install /quiet /norestart" -NoNewWindow -Wait
-}
-
-Out-Console -Message "Installing KDiff3"
-if (!(Confirm-Kdiff))
-{
-  Start-Process -FilePath "$Kdiff3FolderPath\KDiff3-64bit-Setup_0.9.98-2.exe" -ArgumentList "/S" -NoNewWindow -Wait
-}
-
-Out-Console -Message "Configuring git with KDiff3"
-# See if the config already exists because you can actually put multiple entries in the config, might not be an issue but I don't like it
-# Going to assume if merge.tool does not exist neither does the other
-if (!(Confirm-GitConfig -Command "merge.tool"))
-{
-  Start-Process -FilePath "C:\Program Files\Git\bin\git.exe" -ArgumentList "config --global merge.tool kdiff3" -WindowStyle Minimized -Wait
-  Start-Process -FilePath "C:\Program Files\Git\bin\git.exe" -ArgumentList "config --global mergetool.kdiff3.path C:\Progra~1\KDiff3\kdiff3.exe" -WindowStyle Minimized -Wait
+  Start-Process -FilePath "$SsmsFolderPath\$SsmsExecutable" -ArgumentList "/install /quiet /norestart" -NoNewWindow -Wait
 }
 
 Write-Host "*** You may need to open SQL Server Management Studio (SSMS) with Admin Rights if you get a logon error ***`n" -BackgroundColor Black -ForegroundColor Cyan
